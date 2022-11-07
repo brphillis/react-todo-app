43b771b4327c08baf65f71f4ce6aefba21565f50
Brock Phillis <Brockphillis1992@gmail.com>
2022-11-07 12:33:48 +1000

    react-todo-app

---
+++ src/components/TodoForm.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect, useRef } from "react";
+import { BsPlusCircleFill } from "react-icons/bs";
+
+function TodoForm(props) {
+  const [input, setInput] = useState("");
+
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  });
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    props.onSubmit({
+      id: Math.floor(Math.random() * 10000),
+      text: input,
+    });
+
+    setInput(" ");
+  };
+
+  return (
+    <form className="todo-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="type something..."
+        value={input}
+        name="text"
+        className="todo-input"
+        onChange={handleChange}
+        ref={inputRef}
+      />
+      <button className="todo-button">
+        <BsPlusCircleFill />
+      </button>
+    </form>
+  );
+}
+
+export default TodoForm;
